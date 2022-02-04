5418b020dc8381c96af5fb451cf7b29149ccb21c
leesiyoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2022-02-04 09:50:59 +0900

    styled-component added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,37 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import styles from "./Movie.module.css";
+
+const Wrapper = styled.div`
+  background-color: white;
+  margin-bottom: 70px;
+  font-weight: 300;
+  padding: 20px;
+  border-radius: 5px;
+  color: #adaeb9;
+  display: grid;
+  grid-template-columns: minmax(150px, 1fr) 2fr;
+  grid-gap: 20px;
+  text-decoration: none;
+  color: inherit;
+  box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
+    0 8px 16px -8px rgba(0, 0, 0, 0.3), 0 -6px 16px -6px rgba(0, 0, 0, 0.025);
+`;
+const CoverImg = styled.img`
+  position: relative;
+  top: -50px;
+  max-width: 150px;
+  width: 100%;
+  margin-right: 30px;
+  box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),
+    0 18px 36px -18px rgba(0, 0, 0, 0.3), 0 -12px 36px -8px rgba(0, 0, 0, 0.025);
+`;
 
 function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div className={styles.movie}>
-      <img src={coverImg} alt={title} className={styles.movie__img} />
+    <Wrapper>
+      <CoverImg src={coverImg} alt={title} />
       <div>
         <h2 className={styles.movie__title}>
           <Link to={`/movie/${id}`}>{title}</Link>
@@ -18,7 +44,7 @@ function Movie({ id, coverImg, title, year, summary, genres }) {
           ))}
         </ul>
       </div>
-    </div>
+    </Wrapper>
   );
 }
 
